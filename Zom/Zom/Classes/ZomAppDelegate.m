7867fcd1234d206045cbc30daa4e3be39e99c3bb
N-Pex <npexdev@gmail.com>
2015-11-16 09:30:55 +0100

    Support for handling universal links for invites
    Still need to add associated domains for this to work correctly

--- Zom/Zom/Classes/ZomAppDelegate.m
+++ Zom/Zom/Classes/ZomAppDelegate.m
@@ -9,7 +9,13 @@
 #import "ZomAppDelegate.h"
 #import "ZomTheme.h"
 #import "Zom-Swift.h"
+#import <ChatSecureCore/NSURL+ChatSecure.h>
+#import <ChatSecureCore/OTRAppDelegate.h>
 
+@interface OTRAppDelegate (Zom)
+- (void)handleInvite:(NSString *)jidString fingerprint:(NSString *)fingerprint;
+@end
+    
 @implementation ZomAppDelegate
 
 #pragma mark - Theming
@@ -23,6 +29,38 @@
 - (Class) messagesViewControllerClass
 {
     return [ZomMessagesViewController class];
+}
+
+#pragma mark - Universal Links
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler
+{
+    if (userActivity.activityType == NSUserActivityTypeBrowsingWeb) {
+        if (![self handleUniversalLink:userActivity.webpageURL]) {
+            [[UIApplication sharedApplication] openURL:userActivity.webpageURL];
+        }
+    }
+    return TRUE;
+}
+
+- (bool)handleUniversalLink:(NSURL *)url {
+    
+    NSURLComponents *components = [[NSURLComponents alloc] initWithURL:url resolvingAgainstBaseURL:YES];
+    NSString *host = components.host;
+    if ([host isEqualToString:@"zom.im"] && [url otr_isInviteLink]) {
+
+        __block NSString *username = nil;
+        __block NSString *fingerprint = nil;
+        [url otr_decodeShareLink:^(NSString *uName, NSString *fPrint) {
+            username = uName;
+            fingerprint = fPrint;
+        }];
+        if (username.length) {
+            [super handleInvite:username fingerprint:fingerprint];
+        }
+        return true;
+    }
+    return false;
 }
 
 
