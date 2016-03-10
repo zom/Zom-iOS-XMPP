0141e299992b001341126de5f762af6e6b920802
N-Pex <npexdev@gmail.com>
2016-03-10 11:05:31 +0100

    Update from upstream and fix build problems

--- Zom/Zom/Classes/View Controllers/Onboarding/ZomIntroViewController.swift
+++ Zom/Zom/Classes/View Controllers/Onboarding/ZomIntroViewController.swift
@@ -14,20 +14,11 @@ public class ZomIntroViewController: OTRWelcomeViewController {
     public override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.interactivePopGestureRecognizer?.delegate = nil
-        self.completionBlock = { (account, error) -> Void in
-            if (error == nil) {
-                if (account != nil) {
-                    OTRInviteViewController.showInviteFromVC(self, withAccount: account)
-                }
-            }
-        }
     }
     
     // MARK: - Navigation
     override public func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "useExistingAccountSegue" {
-            let vc: OTRExistingAccountViewController = segue.destinationViewController as! OTRExistingAccountViewController
-            vc.completionBlock = self.completionBlock
         }
         super.prepareForSegue(segue, sender:sender)
     }
