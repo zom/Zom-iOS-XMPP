c06adeb2a84879eb6bd88eab86e43bf13cf4cb21
N-Pex <npexdev@gmail.com>
2016-04-28 12:16:32 +0200

    Update from upstream

--- Zom/Zom/Classes/View Controllers/ZomComposeViewController.swift
+++ Zom/Zom/Classes/View Controllers/ZomComposeViewController.swift
@@ -11,18 +11,20 @@ import ChatSecureCore
 
 public class ZomComposeViewController: OTRComposeViewController {
     
-    public override func addBuddy(accountsAbleToAddBuddies: [AnyObject]!) {
-        if (accountsAbleToAddBuddies.count > 0)
-        {
-            let storyboard = UIStoryboard(name: "AddBuddy", bundle: nil)
-            var vc:UIViewController? = nil
-            if (accountsAbleToAddBuddies.count == 1) {
-                vc = storyboard.instantiateViewControllerWithIdentifier("addNewBuddy")
-                (vc as! ZomAddBuddyViewController).account = accountsAbleToAddBuddies[0] as? OTRAccount
-                self.navigationController?.pushViewController(vc!, animated: true)
-            } else {
-                vc = storyboard.instantiateInitialViewController()
-                self.navigationController?.presentViewController(vc!, animated: true, completion: nil)
+    public override func addBuddy(accountsAbleToAddBuddies: [OTRAccount]?) {
+        if let accounts = accountsAbleToAddBuddies {
+            if (accounts.count > 0)
+            {
+                let storyboard = UIStoryboard(name: "AddBuddy", bundle: nil)
+                var vc:UIViewController? = nil
+                if (accounts.count == 1) {
+                    vc = storyboard.instantiateViewControllerWithIdentifier("addNewBuddy")
+                    (vc as! ZomAddBuddyViewController).account = accounts[0]
+                    self.navigationController?.pushViewController(vc!, animated: true)
+                } else {
+                    vc = storyboard.instantiateInitialViewController()
+                    self.navigationController?.presentViewController(vc!, animated: true, completion: nil)
+                }
             }
         }
 
