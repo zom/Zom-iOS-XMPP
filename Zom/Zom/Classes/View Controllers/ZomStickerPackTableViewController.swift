cf0988b2c44247ccce79fef0c3f544b641263fcd
N-Pex <npexdev@gmail.com>
2016-05-26 13:00:20 +0200

    Swipe between sticker sets
    Fixes issue #113.

--- Zom/Zom/Classes/View Controllers/ZomStickerPackTableViewController.swift
+++ Zom/Zom/Classes/View Controllers/ZomStickerPackTableViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 import ChatSecureCore
 import Photos
 
-public class ZomStickerPackTableViewController: UITableViewController {
+public class ZomStickerPackTableViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
 
     private var stickerPacks: Array<String> = [];
+    private(set) lazy var orderedViewControllers: [UIViewController] = []
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,36 +25,80 @@ public class ZomStickerPackTableViewController: UITableViewController {
         } catch {
             print(error)
         }
-    }
-    
-    public override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return stickerPacks.count
-    }
-    
-    public override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("stickerPackCell")!
-        cell.textLabel!.text = String(format: "%@", stickerPacks[indexPath.row])
-        return cell
-    }
-
-    // Mark - UIViewController
-    
-    public override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Create view controllers
+        for stickerPack in stickerPacks {
+            let vc:ZomPickStickerViewController = self.storyboard?.instantiateViewControllerWithIdentifier("pickStickerViewController") as! ZomPickStickerViewController
+            vc.stickerPack = stickerPack
+            orderedViewControllers.append(vc)
+        }
+        dataSource = self
+        delegate = self
         
-        if (segue.destinationViewController is ZomPickStickerViewController) {
-        let cell:UITableViewCell? = (sender as? UITableViewCell)!
-        if (cell == nil) {
-            return
+        if let firstViewController:ZomPickStickerViewController = orderedViewControllers.first as? ZomPickStickerViewController {
+            self.navigationItem.title = firstViewController.stickerPack
+            setViewControllers([firstViewController],
+                               direction: .Forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    public func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
         }
         
-        let vc:ZomPickStickerViewController = segue.destinationViewController as! ZomPickStickerViewController
-    
-        let indexPath:NSIndexPath = self.tableView.indexPathForCell(cell!)!
-        let stickerPack:String = stickerPacks[indexPath.row]
-        vc.stickerPack = stickerPack
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
         }
-        super.prepareForSegue(segue, sender: sender)
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
     }
     
+    public func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    public func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    public func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            firstViewControllerIndex = orderedViewControllers.indexOf(firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+    
+    public func pageViewController(pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if let vc:ZomPickStickerViewController = pageViewController.viewControllers?[0] as? ZomPickStickerViewController {
+            self.navigationItem.title = vc.stickerPack
+        }
+    }
 }
