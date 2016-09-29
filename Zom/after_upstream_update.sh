3a45dd4dcc1a338f67211fb0110be8cee29cd753
N-Pex <npexdev@gmail.com>
2016-09-29 15:52:47 +0200

    Need to set embed libraries to false in submodules

--- Zom/after_upstream_update.sh
+++ Zom/after_upstream_update.sh
@@ -5,3 +5,5 @@ oldTeam=$(awk -F '=|;' '/DevelopmentTeam/ {print $2; exit}' "$projectfile")
 newTeam="9SV9LPRC42"
 sed -i '' "s/$oldTeam/ $newTeam/g" "$projectfile"
 echo "Changed team from $oldTeam to $newTeam"
+sed -i '' "s/ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES = YES;/ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES = NO;/g" "$projectfile"
+echo "Changed ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES to NO"
