1cc972cb3b2157380267a817b525a43a6849a24f
Chris Ballinger <chrisballinger@gmail.com>
2017-01-27 18:03:50 -0800

    Merging upstream. There is a Swift segmentation fault in the compiler now. Great!

--- Zom/Zom/Zom-Bridging-Header.h
+++ Zom/Zom/Zom-Bridging-Header.h
@@ -1,46 +1,8 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-#import <ChatSecureCore/ChatSecureCore.h>
-#import <ChatSecureCore/OTRLanguageListSettingViewController.h>
-#import <ChatSecureCore/OTRSettingsManager.h>
-#import <ChatSecureCore/OTRAccountsManager.h>
-#import <ChatSecureCore/OTRLanguageManager.h>
-#import <ChatSecureCore/OTRLanguageSetting.h>
-#import <ChatSecureCore/OTRMessagesHoldTalkViewController.h>
-#import <ChatSecureCore/OTRAttachmentPicker.h>
-#import <ChatSecureCore/OTRExistingAccountViewController.h>
-#import <ChatSecureCore/OTRInviteViewController.h>
-#import <ChatSecureCore/OTRConversationViewController.h>
-#import <ChatSecureCore/OTRDatabaseManager.h>
-#import <ChatSecureCore/OTRDatabaseView.h>
-#import <ChatSecureCore/OTRChooseAccountViewController.h>
-#import <ChatSecureCore/OTRNewBuddyViewController.h>
-#import <ChatSecureCore/OTRDatabaseUnlockViewController.h>
-#import <ChatSecureCore/OTRProtocolManager.h>
-#import <ChatSecureCore/OTRComposeViewController.h>
-#import <ChatSecureCore/OTRBuddyInfoCell.h>
-#import <ChatSecureCore/OTRTitleSubtitleView.h>
-#import <ChatSecureCore/OTRSettingsGroup.h>
-#import <ChatSecureCore/OTRFeedbackSetting.h>
-#import <ChatSecureCore/OTRDonateSetting.h>
-#import <ChatSecureCore/OTRViewSetting.h>
-#import <ChatSecureCore/OTRSettingsViewController.h>
-#import <QRCodeReaderViewController/QRCodeReaderViewController-umbrella.h>
-#import <ChatSecureCore/OTRMessage.h>
-#import <ChatSecureCore/OTRMessage+JSQMessageData.h>
-#import <ChatSecureCore/OTRMediaItem.h>
-#import <ChatSecureCore/OTRTheme.h>
-#import <ChatSecureCore/OTRBuddy.h>
-#import <ChatSecureCore/OTRXMPPBuddy.h>
-#import <OTRAssets/OTRAssets.h>
-#import <OTRAssets/OTRStrings.h>
-#import <ZXingObjC/ZXingObjC-umbrella.h>
-#import <KVOController/FBKVOController.h>
+
 #import "ZomTheme.h"
 #import "ZomAppDelegate.h"
 #import "ZomOverrides.h"
-#import <MessageUI/MessageUI.h>
-#import <BButton/NSString+FontAwesome.h>
-#import <JSQMessagesViewController/JSQMessagesInputToolbar.h>
 #import "OTRAssets+ZomLanguageHandling.h"
