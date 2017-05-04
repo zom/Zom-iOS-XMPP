fff4bd1cc73207fa03d1e0436cc7da6bce22b45a
N-Pex <npexdev@gmail.com>
2017-05-04 15:50:28 +0200

    Tibetan strings fix (don't use attributed string)

--- Zom/Zom/Classes/Views/ZomMigrationInfoHeaderView.swift
+++ Zom/Zom/Classes/Views/ZomMigrationInfoHeaderView.swift
@@ -10,19 +10,39 @@ import UIKit
 import ChatSecureCore
 
 public class ZomMigrationInfoHeaderView: MigrationInfoHeaderView {
+    var initialized: Bool = false
     @IBOutlet public var imageView: UIImageView!
     @IBOutlet public var autoMigrateButton: UIButton!
     @IBOutlet public var assistedMigrateButton: UIButton!
     @IBOutlet public var workingButton: UIButton!
     @IBOutlet public var doneButton: UIButton!
-    @IBOutlet public var infoLabel: UIView!
+    @IBOutlet public var infoLabel: UILabel!
     @IBOutlet public var doneLabel: UIView!
     
     public override func layoutSubviews() {
+        if !initialized, let text = infoLabel.text, let idxStart = text.range(of: "[["), let idxEnd = text.range(of: "]]") {
+            let start = text.distance(from: text.startIndex, to: idxStart.lowerBound)
+            let end = text.distance(from: text.startIndex, to: idxEnd.lowerBound) - 2
+            if start < end {
+                // Create attributed string
+                var textCopy = text
+                textCopy.removeSubrange(idxEnd)
+                textCopy.removeSubrange(idxStart)
+                let length = end - start
+                let mutableAttrString = NSMutableAttributedString(string: textCopy)
+                let range = NSRange(location: start, length: length)
+                mutableAttrString.addAttribute(NSUnderlineStyleAttributeName, value: NSUnderlineStyle.styleSingle.rawValue, range: range)
+                mutableAttrString.addAttribute(NSFontAttributeName, value: UIFont.boldSystemFont(ofSize: infoLabel.font.pointSize), range: range)
+                infoLabel.attributedText = mutableAttrString
+            }
+        }
         super.layoutSubviews()
-        autoMigrateButton.backgroundColor = UIColor.yellow
-        workingButton.backgroundColor = UIColor(white: 0, alpha: 0.3)
-        workingButton.tintColor = UIColor.white
-        doneButton.backgroundColor = UIColor.white
+        if !initialized {
+            autoMigrateButton.backgroundColor = UIColor.yellow
+            workingButton.backgroundColor = UIColor(white: 0, alpha: 0.3)
+            workingButton.tintColor = UIColor.white
+            doneButton.backgroundColor = UIColor.white
+            initialized = true
+        }
     }
 }
