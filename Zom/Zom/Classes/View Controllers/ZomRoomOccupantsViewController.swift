591ea89710bdfefb9886d45d1591ce61a71bc70f
N-Pex <npexdev@gmail.com>
2017-07-31 11:45:21 +0200

    More work on group info view

--- Zom/Zom/Classes/View Controllers/ZomRoomOccupantsViewController.swift
+++ Zom/Zom/Classes/View Controllers/ZomRoomOccupantsViewController.swift
@@ -13,9 +13,15 @@ open class ZomRoomOccupantsViewController : OTRRoomOccupantsViewController {
     
     @IBOutlet weak var tableViewHeader:UIView!
     @IBOutlet weak var largeAvatarView:UIImageView!
+    @IBOutlet weak var qrCodeButton:UIButton!
     
     @IBOutlet weak var groupInfoTableView: UITableView!
     var groupInfoTableViewDataSource:GroupInfoTableDataSource?
+    
+    var groupTableViewDataSource:GroupTableDataSource?
+    
+    @IBOutlet weak var groupFooterTableView: UITableView!
+    var groupFooterTableViewDataSource:GroupFooterTableDataSource?
     
     public override init(databaseConnection:YapDatabaseConnection, roomKey:String) {
         super.init(databaseConnection: databaseConnection, roomKey: roomKey)
@@ -27,11 +33,38 @@ open class ZomRoomOccupantsViewController : OTRRoomOccupantsViewController {
     
     open override func viewDidLoad() {
         super.viewDidLoad()
+        self.groupTableViewDataSource = GroupTableDataSource(dataSource: self.tableView.dataSource!)
+        self.tableView.dataSource = self.groupTableViewDataSource
         self.groupInfoTableViewDataSource = GroupInfoTableDataSource(roomKey: self.roomKey!)
         groupInfoTableView.dataSource = self.groupInfoTableViewDataSource
         groupInfoTableView.delegate = self.groupInfoTableViewDataSource
+        self.groupFooterTableViewDataSource = GroupFooterTableDataSource(roomKey: self.roomKey!)
+        groupFooterTableView.dataSource = self.groupFooterTableViewDataSource
+        groupFooterTableView.delegate = self.groupFooterTableViewDataSource
         let image = OTRGroupAvatarGenerator.avatarImage(withUniqueIdentifier: self.roomKey!, width: Int(largeAvatarView.frame.width), height: Int(largeAvatarView.frame.height))
         largeAvatarView.image = image
+        qrCodeButton.backgroundColor = UIColor.white //reset this, set by appearance proxy
+    }
+    
+    class GroupTableDataSource: NSObject, UITableViewDataSource {
+        let superSource:UITableViewDataSource
+        
+        init(dataSource:UITableViewDataSource) {
+            self.superSource = dataSource
+            super.init()
+        }
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return superSource.numberOfSections!(in:tableView)
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return superSource.tableView(tableView, numberOfRowsInSection:section)
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return superSource.tableView(tableView, cellForRowAt:indexPath)
+        }
     }
     
     class GroupInfoTableDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
@@ -103,4 +136,51 @@ open class ZomRoomOccupantsViewController : OTRRoomOccupantsViewController {
             }
         }
     }
+    
+    class GroupFooterTableDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
+        let roomKey:String
+        
+        enum GroupFooterRows: Int {
+            case leave
+            static let allValues = [leave]
+        }
+        
+        init(roomKey:String) {
+            self.roomKey = roomKey
+            super.init()
+        }
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return GroupFooterRows.allValues.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            var cell:UITableViewCell?
+            if let row = GroupFooterRows(rawValue: indexPath.row) {
+                switch row {
+                case .leave:
+                    cell = tableView.dequeueReusableCell(withIdentifier: "cellGroupLeave", for: indexPath)
+                }
+            }
+            return cell!
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+            guard let row = GroupFooterRows(rawValue: indexPath.row) else {
+                return
+            }
+            switch row {
+            case .leave:
+                print("leave")
+            default:
+                print("ignore")
+            }
+        }
+    }
+
 }
