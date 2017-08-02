738178e4cbdb3e52adff309ba1cc02dae55a8b73
N-Pex <npexdev@gmail.com>
2017-08-02 11:38:18 +0200

    Remove nav bar for group profile view

--- Zom/Zom/Classes/View Controllers/ZomRoomOccupantsViewController.swift
+++ Zom/Zom/Classes/View Controllers/ZomRoomOccupantsViewController.swift
@@ -14,6 +14,7 @@ open class ZomRoomOccupantsViewController : OTRRoomOccupantsViewController {
     @IBOutlet weak var tableViewHeader:UIView!
     @IBOutlet weak var largeAvatarView:UIImageView!
     @IBOutlet weak var qrCodeButton:UIButton!
+    var navigationBarShadow:UIImage?
     
     @IBOutlet weak var groupInfoTableView: UITableView!
     var groupInfoTableViewDataSource:GroupInfoTableDataSource?
@@ -31,9 +32,26 @@ open class ZomRoomOccupantsViewController : OTRRoomOccupantsViewController {
         super.init(coder: aDecoder)
     }
     
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationBarShadow = self.navigationController?.navigationBar.shadowImage
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.barTintColor = UIColor.clear
+        self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+        self.navigationController?.navigationBar.isTranslucent = true
+    }
+    
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.barTintColor = UINavigationBar.appearance().barTintColor
+        self.navigationController?.navigationBar.backgroundColor = UINavigationBar.appearance().backgroundColor
+        self.navigationController?.navigationBar.shadowImage = self.navigationBarShadow
+    }
+    
     open override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.tableView.register(OTRBuddyInfoCell.self, forCellReuseIdentifier: "buddyCell")
         
         self.groupTableViewDataSource = GroupTableDataSource(parent: self, dataSource: self.tableView.dataSource!)
