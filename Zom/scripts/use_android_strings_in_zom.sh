f499445e5ade37a20e97c712e9f9c4e9e726fc93
N-Pex <npexdev@gmail.com>
2017-08-03 09:57:48 +0200

    Remove old script and rename new one

--- Zom/scripts/use_android_strings_in_zom.sh
+++ Zom/scripts/use_android_strings_in_zom.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 #######
-# A Script to sync strings between Android and iOS projects, or rather: use Android strings in iOS storyboards
-# Created 2016 by N-Pex
+# A Script to use Android strings in iOS projects
+# Created 2017 by N-Pex
 #
 # The process is the following:
 #
-# Step 1 - Search through all lproj directories, finding all localized strings files matching the given storyboard.
-# Step 2 - Make sure corresponding Android strings file exists.
-# Step 3 - Extract strings from base storyboard and convert to utf-8. Split them into key-value pairs.
-# Step 4 - Extract strings from base android strings file, split into key-value pairs.
-# Step 5 - Match translated strings iOS<->Android. If they match, store the id mapping between them.
-# Step 6 - Go through the iOS strings files, get the corresponding Android strings file and use the id mapping built in Step 5 to update the strings.
-
-base_file_ios=""
+# - Export an XLIFF for the base language
+# - Find out what languages we have (by enumerating all *.lproj directories)
+# - Export an XLIFF for each language found
+# - Parse iOS base file to get iOS id <-> English string mapping
+# - Parse Android base file(s) to get Android id <-> English string mapping
+# - For each language:
+#     parse iOS file
+#     parse corresponding Android file(s)
+#     use above mapping to update strings in the language xliff file
+#     import the updated xliff back into the ios project
+#
+#
+#
+# This script requires xidel, found at http://videlibri.sourceforge.net/xidel.html
+#
+project_dir=""
+project_file=""
 android_input_files=()
+xidel=""
 
 function addAndroidInputFile {
     local count=0
@@ -26,9 +36,11 @@ function addAndroidInputFile {
 }
 
 function showUsageAndExit {
-    echo "Usage: $0 -s <storyboardfile> -i <android_strings_file> [-i <android_strings_file>...]";
+    echo "Usage: $0 -x <path_to_xidel_binary> -d <project_dir> -p <project_file> -i <android_strings_file> [-i <android_strings_file>...]";
     echo
-    echo "example: $0 -s ./OTRResources/Interface/Base.lproj/Onboarding.storyboard -i ../../Zom-Android/app/src/main/res/values/zomstrings.xml -i ../../Zom-Android/app/src/main/res/values/strings.xml"
+    echo "example: $0 -x ~/Downloads/xidel -d ../Zom -p ../Zom.xcodeproj -i ../../../Zom-Android/app/src/main/res/values/zomstrings.xml -i ../../../Zom-Android/app/src/main/res/values/strings.xml"
+    echo
+    echo "Xidel can be found here: http://videlibri.sourceforge.net/xidel.html"
     exit
 }
 
@@ -42,8 +54,16 @@ do
 	-h|--help)
 	    showUsageAndExit
 	    ;;
-	-s|--storyboard)
-	    base_file_ios="$2"
+	-x|--xidel)
+	    xidel="$2"
+	    shift
+	    ;;
+	-d|--dir)
+	    project_dir="$2"
+	    shift
+	    ;;
+	-p|--project)
+	    project_file="$2"
 	    shift # past argument
 	    ;;
 	-i|--input)
@@ -58,13 +78,29 @@ echo
 echo "Checking indata..."
 echo
 
-# Check that storyboard file exists
-if [ ! -f "$base_file_ios" ]; then
-    echo "storyboard file not found!"
+# Check that the xidel tool exists
+if [ ! -f "$xidel" ]; then
+    echo "xidel tool not found!"
     echo
     showUsageAndExit
 fi
-echo "Storyboard file $base_file_ios found"
+echo "Using xidel from $xidel"
+
+# Check that project dir exists
+if [ ! -d "$project_dir" ]; then
+    echo "project dir not found!"
+    echo
+    showUsageAndExit
+fi
+echo "Project dir $project_dir found"
+
+# Check that project file exists
+if [ ! -d "$project_file" ]; then
+    echo "project file not found!"
+    echo
+    showUsageAndExit
+fi
+echo "Project file $project_file found"
 
 # Check Android input files
 android_count=0
@@ -103,39 +139,39 @@ function getAndroidLanguageCodeFromiOSLanguageCode {
     echo "-$id"
 }
 
-# Step 1 - Search through all lproj directories, finding all localized strings files matching the given storyboard.
+# Export XLIFF files for Base
+#
+#
+echo "Export base XLIFF file"
+if [ -d "/tmp/ZomTranslations" ]; then
+    rm -rf "/tmp/ZomTranslations"
+fi
+xcodebuild -exportLocalizations -localizationPath /tmp/ZomTranslations -exportLanguage Base -project "$project_file"
+
+# Export for all others
 #
 #
 languages=()
 ios_files=()
 languages_index=0
-base_dir_ios="${base_file_ios%%/Base.lproj*}"
+base_dir_ios="$project_dir/"
 echo "Base dir is $base_dir_ios"
 for languageDir in $(find $base_dir_ios -depth 1 -name "*.lproj" -print)
 do
-    languageDir="${languageDir%%.lproj}" # strip .lproj
-    languageDir="${languageDir##*/}" # strip everything to the last path character.lproj
-    #if [[ "$languageDir" == "Base" ]]
-    #then
-#	continue
-#    fi
-
-    echo "$LanguageFile"
-
-    # Does the file exist for this language?
-    languageFile="${base_file_ios/Base.lproj/${languageDir}.lproj}"
-    languageFile="${languageFile/.storyboard/.strings}"
-    languageFile="${languageFile/.xib/.strings}"
-    if [ ! -f $languageFile ]; then
-	echo "Storyboard strings file not found for language $languageDir"
+    language="${languageDir%%.lproj}" # strip .lproj
+    language="${language##*/}" # strip everything to the last path character.lproj
+    if [[ "$language" == "Base" ]]
+    then
 	continue
     fi
-    languages[languages_index]="$languageDir"
-    ios_files[languages_index]="$languageFile"
+    languages[languages_index]="$language"
     ((languages_index++))
+    echo "Export XLIFF for $language"
+    xcodebuild -exportLocalizations -localizationPath /tmp/ZomTranslations -exportLanguage "$language" -project "$project_file"
+    perl -i~ -0777 -pe "s/<file original=\"[^\.]+\.plist\"(.|\n)*?<\/file>\s*//g" "/tmp/ZomTranslations/${language}.xliff"
 done
 
-# Step 2 - Make sure corresponding Android strings file exists
+# Make sure corresponding Android strings file exists
 #
 #
 languages_index=0
@@ -150,16 +186,9 @@ do
     fi
 done
 
-# Step 3 - Extract strings from base storyboard and convert to utf-8. Split them into key-value pairs.
+# Extract ids and English translations from Base.xliff
 #
 #
-ibtool --export-strings-file /tmp/Localizable.strings "$base_file_ios"
-iconv -f utf-16 -t utf-8 /tmp/Localizable.strings >/tmp/Localizable.strings.utf8 || {
-    echo "Failed to convert to utf-8"
-    exit 1
-}
-default_strings_file="/tmp/Localizable.strings.utf8"
-
 base_keys=()
 base_translations=()
 android_base_keys=()
@@ -172,7 +201,7 @@ ios_values=()
 
 
 function addBaseMapping {
-    count=0
+    local count=0
     while [ "x${base_keys[count]}" != "x" ]
     do
 	((count++))
@@ -182,7 +211,7 @@ function addBaseMapping {
 }
 
 function addAndroidMapping {
-    count=0
+    local count=0
     while [ "x${android_keys[count]}" != "x" ]
     do
 	((count++))
@@ -192,13 +221,14 @@ function addAndroidMapping {
 }
 
 function addiOSMapping {
-    count=0
+    local count=0
     while [ "x${ios_keys[count]}" != "x" ]
     do
 	((count++))
     done
-    ios_keys[count]="$1"
-    ios_values[count]="$2"
+    ios_ids[count]="$1"
+    ios_keys[count]="$2"
+    ios_values[count]="$3"
 }
 
 function parseAndroidStringsFile {
@@ -217,14 +247,21 @@ function parseAndroidStringsFile {
 function parseiOSStringsFile {
     local filePath="$1"
     echo "Parsing iOS file $filePath"
+    echo "Parsing XLIFF file $filePath"
+
+sep="####"
+$xidel --xpath "//file[contains(@original,'.storyboard') or contains(@original,'.xib') or contains(@original,'.strings')]//trans-unit/(concat(@id,'$sep',source/text(),'$sep',target/text()))" "$filePath" > /tmp/parsed.xml
+
+    ios_ids=()
     ios_keys=()
     ios_values=()
     while read -r line || [[ -n $line ]]; do
-	sep=" = "
 	case $line in
 	    (*"$sep"*)
-	    key=${line%%"$sep"*}
-	    value=${line#*"$sep"}
+	    id=${line%%"$sep"*}
+            keyval=${line#*"$sep"}
+	    key=${keyval%%"$sep"*}
+	    value=${keyval#*"$sep"}
 	    ;;
 	    (*)
 	    key=
@@ -232,15 +269,10 @@ function parseiOSStringsFile {
 	    ;;
 	esac
 	if [ "$key" != "" ]; then
-	    # Cleanup key and value by removing quotes
-	    key=${key#\"}
-	    key=${key%\"}
-	    value=${value#\"}
-	    value=${value%\";}
-	    echo "Adding mapping $key --> $value"
-	    addiOSMapping "$key" "$value"
+	    #echo "Adding mapping $key --> $value"
+	    addiOSMapping "$id" "$key" "$value"
 	fi
-    done < <(sed -n '/^\".*;$/p' "$filePath")
+    done < "/tmp/parsed.xml"
 }
 
 function findAndroidTranslation {
@@ -269,12 +301,18 @@ function findAndroidId {
     done
 }
 
+sep="####"
+$xidel --xpath "//file[contains(@original,'.storyboard') or contains(@original,'.xib') or contains(@original,'.strings')]//trans-unit/(concat(@id,'$sep',source/text(),'$sep',target/text()))" "/tmp/ZomTranslations/Base.xliff" > /tmp/base.xml
+
 while read -r line || [[ -n $line ]]; do
-    sep=" = "
     case $line in
 	(*"$sep"*)
-	key=${line%%"$sep"*}
-	value=${line#*"$sep"}
+	id=${line%%"$sep"*}
+	keyval=${line#*"$sep"}
+	key=${keyval%%"$sep"*}
+	value=${keyval#*"$sep"}
+	value=${key}
+	key=${id}
 	;;
 	(*)
 	key=
@@ -283,22 +321,13 @@ while read -r line || [[ -n $line ]]; do
     esac
     if [ "$key" != "" ]; then
 	    # Cleanup key and value by removing quotes
-	key=${key#\"}
-	key=${key%\"}
-	value=${value#\"}
-	value=${value%\";}
 	echo "Adding base mapping $key --> $value"
 	addBaseMapping "$key" "$value"
     fi
-done < <(sed -n '/^\".*;$/p' "$default_strings_file")
+done < "/tmp/base.xml"
 
 
-
-
-
-
-
-# Step 4 - Extract strings from base android strings file, split into key-value pairs.
+# Extract strings from base android strings file, split into key-value pairs.
 #
 #
 android_keys=()
@@ -312,7 +341,7 @@ do
 done
 
 
-# Step 5 - Match translated strings iOS<->Android. If they match, store the id mapping between them.
+# Match translated strings iOS<->Android. If they match, store the id mapping between them.
 #
 #
 i_translation=0
@@ -331,31 +360,27 @@ do
     done
     
     if [ "$id" == "" ]; then
-	echo "Could not find Android string id for $term"
+	#echo "Could not find Android string id for $term"
 	#exit 1
 	android_base_keys[i_translation]=""
     else
-	#echo "Lookup for $term is $id"
+	echo "Lookup for $term is $id"
 	android_base_keys[i_translation]="$id"
     fi
     ((i_translation++))
 done
 
-
-# Step 6 - Go through the iOS strings files, get the corresponding Android strings file and use the id mapping built in Step 5 to update the strings.
+# Go through the iOS strings files, get the corresponding Android strings file and use the id mapping built in previous step to update the strings.
 #
 #
 languages_index=0
 while [ "x${languages[languages_index]}" != "x" ]
 do
     language="${languages[languages_index]}"
-    ios_file="${ios_files[languages_index]}"
+    ios_file="/tmp/ZomTranslations/${language}.xliff"
     echo "Processing language: $language ios_file $ios_file"
     ((languages_index++))
     language_suffix=$(getAndroidLanguageCodeFromiOSLanguageCode "$language")
-    
-    # Copy a fresh strings file to our target ios language file
-    cp /tmp/Localizable.strings.utf8 "$ios_file"
     
     android_keys=()
     android_values=()
@@ -384,6 +409,7 @@ do
 	    if [ "$translation" == "" ]; then
 		echo "Language $language: missing translation for id $android_key."
 	    else
+		#echo "Translation for $android_key is $translation"
 		ios_values[count]="$translation"
 	    fi
 	fi
@@ -391,22 +417,30 @@ do
     done
     
     # Second pass: update the actual file
+    echo "Update file $ios_file"
     count=0
     while [ "x${ios_keys[count]}" != "x" ]
     do
+	id="${ios_ids[count]}"
 	key="${ios_keys[count]}"
 	value="${ios_values[count]}"
-	value="${value//\&/\\&}"
-	echo "Update file $ios_file with $key -> $value"
-	value="${value/\\/\\\\}"
-	command="s/$key\" =.*/$key\" = \"$value\";/"
-	sed -e "$command" -i "" "$ios_file"
+	if [ "$value" != "" ]; then
+	    #echo "Update file $ios_file with $id: $key -> $value"
+	    value="${value//&/&amp;}"
+	    value="${value//</&lt;}"
+	    value="${value//>/&gt;}"
+	    export translation=${value}
+	    perl -i~ -0777 -pe "s/(<trans-unit id=\"$id\">\s+<source>.*<\/source>)(\s+<target>.*<\/target>)?/\$1\n        <target>\$ENV{translation}<\/target>/g" "$ios_file"
+	fi
 	((count++))
     done
+
+    # Update the xcode project
+    echo "Update XCode project"
+    rm "${project_dir}/${language}.lproj/Localizable.strings"
+    xcodebuild -importLocalizations -localizationPath "$ios_file" -project "$project_file"
     
-    if [ "$language" == "Base" ]; then
-	mv "$ios_file" "$ios_file.utf8"
-	iconv -f utf-8 -t utf-16 "$ios_file.utf8" > "$ios_file"
-	rm "$ios_file.utf8"
-    fi
+    # Clean up by removing some strange extra files created by xcode during the import process
+    fgrep "IDELocalizationWork Temporary File" -l "${project_dir}/${language}.lproj/*.strings" | xargs -I {} rm {}
 done
+
