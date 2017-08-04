44a7cd47c27dfbcf3d8d15a47a15c4f7dc971b5c
N-Pex <npexdev@gmail.com>
2017-08-04 14:42:32 +0200

    Clean up after moving stuff to upstream

--- Zom/Zom/Classes/View Controllers/Messages View Controller/ZomGroupMessagesViewController.swift
+++ Zom/Zom/Classes/View Controllers/Messages View Controller/ZomGroupMessagesViewController.swift
@@ -14,11 +14,5 @@ import BButton
 import AFNetworking
 
 open class ZomGroupMessagesViewController: OTRMessagesGroupViewController {
-    override open func didSelectOccupantsButton(_ sender: Any!) {
-        let storyboard = UIStoryboard(name: "Zom", bundle: Bundle.main)
-        let vc = storyboard.instantiateViewController(withIdentifier: "roomOccupants") as! ZomRoomOccupantsViewController
-        vc.setupViewHandler(databaseConnection:OTRDatabaseManager.shared.longLivedReadOnlyConnection!, roomKey: self.threadKey!)
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
 }
 
