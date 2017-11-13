5ff6714b8f6817e086c1ec53feb61abd3fa61121
N-Pex <npexdev@gmail.com>
2017-11-13 14:50:31 +0100

    Disable button on done animating
    Issue #436.

--- Zom/Zom/Classes/View Controllers/Messages View Controller/ZomMessagesViewController.swift
+++ Zom/Zom/Classes/View Controllers/Messages View Controller/ZomMessagesViewController.swift
@@ -585,6 +585,7 @@ open class ZomMessagesViewController: OTRMessagesHoldTalkViewController, UIGestu
                         let range = NSRange(location: 0, length: attributedString.length)
                         attributedString.addAttribute(NSAttributedStringKey.foregroundColor, value: ZomAppDelegate.appDelegate.theme.mainThemeColor, range: range)
                         button.setAttributedTitle(attributedString, for: .normal)
+                        button.isUserInteractionEnabled = false
                     } else {
                         self.rotateRefreshView(button, revolutions:revolutions - 1)
                     }
