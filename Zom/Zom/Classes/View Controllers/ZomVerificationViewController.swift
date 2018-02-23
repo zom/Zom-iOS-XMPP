793a800a1567b8e285389bfcbc9fb60e65bb5d82
Benjamin Erhart <be@benjaminerhart.com>
2018-02-23 14:02:37 +0100

    Added success overlay and animation. Added storing of new trust status. Only use OMEMO devices/OTR fingerprints which have trust status `.untrustedNew`.

--- Zom/Zom/Classes/View Controllers/ZomVerificationViewController.swift
+++ Zom/Zom/Classes/View Controllers/ZomVerificationViewController.swift
@@ -11,9 +11,9 @@ import UIKit
  Scene for verification of the latest fingerprint of a buddy.
 
  Use the `convenience init(buddy: OTRBuddy)` or set `self.buddy` immediately after init!
-
- If OMEMO keys exist, the fingerprint of the first one of these will always be used.
- Fallback to the first OTR key only, when no OMEMO key is found.
+ 
+ The first OMEMO key exists, which has trust level `.untrustedNew`, will be used, otherwise the
+ first OTR fingerprint with this trust level will be used.
  */
 class ZomVerificationViewController: UIViewController {
 
@@ -25,7 +25,20 @@ class ZomVerificationViewController: UIViewController {
     @IBOutlet weak var checkmarkImg: UIImageView!
     @IBOutlet weak var matchBt: UIButton!
     @IBOutlet weak var noMatchBt: UIButton!
+    @IBOutlet weak var overlayContainer: UIView!
+    @IBOutlet weak var successCheckmarkImg: UIImageView!
+    @IBOutlet weak var trustedLb: UILabel!
 
+    private var fingerprintContainer: NSObject?
+
+    /**
+     Convenience initializer which also sets `self.buddy` with the given argument.
+
+     You can do this yourself, since `self.buddy` is public, but you need to do this, before
+     `self.viewDidLoad()` was called!
+
+     - parameter buddy: a OTRBuddy with a new OMEMO or OTR fingerprint.
+    */
     @objc convenience init(buddy: OTRBuddy) {
         self.init()
         self.buddy = buddy
@@ -45,25 +58,32 @@ class ZomVerificationViewController: UIViewController {
             comment: "Description for code verification scene")
 
         if let buddy = buddy, let db = OTRDatabaseManager.sharedInstance().readOnlyDatabaseConnection {
-            db.asyncRead() { (transaction: YapDatabaseReadTransaction) in
+            db.asyncRead() { (transaction) in
                 let allOmemoDevices = OTROMEMODevice.allDevices(
                     forParentKey: buddy.uniqueId,
                     collection: type(of: buddy).collection,
                     transaction: transaction
                 ).filter() { (device) -> Bool in
-                    return device.publicIdentityKeyData != nil && device.trustLevel != .removed
+                    return device.publicIdentityKeyData != nil && device.trustLevel == .untrustedNew
                 }
 
                 if let device = allOmemoDevices.first {
+                    self.fingerprintContainer = device
                     self.setFingerprint(device.humanReadableFingerprint)
                 }
                 else if let account = buddy.account(with: transaction) {
-                    let allFingerprints = OTRProtocolManager.encryptionManager.otrKit.fingerprints(
-                        forUsername: buddy.username, accountName: account.username,
-                        protocol: account.protocolTypeString())
+                    let fingerprints = OTRProtocolManager.encryptionManager.otrKit.fingerprints(
+                        forUsername: buddy.username,
+                        accountName: account.username,
+                        protocol: account.protocolTypeString()
+                    ).filter() { (fingerprint) -> Bool in
+                        return fingerprint.trustLevel == .untrustedNew
+                    }
 
-                    if let fingerprint = allFingerprints.first {
-                        self.setFingerprint((fingerprint.fingerprint as NSData).humanReadableFingerprint())
+                    if let fingerprint = fingerprints.first {
+                        self.fingerprintContainer = fingerprint
+                        self.setFingerprint(
+                            (fingerprint.fingerprint as NSData).humanReadableFingerprint())
                     }
                     else {
                         self.setFingerprintError()
@@ -89,18 +109,92 @@ class ZomVerificationViewController: UIViewController {
 
         noMatchBt.titleLabel?.text = NSLocalizedString("Codes don't match",
                                                        comment: "Verification scene button text")
+
+        successCheckmarkImg.image = successCheckmarkImg.image?.withRenderingMode(.alwaysTemplate)
+        successCheckmarkImg.tintColor = UIColor.white
+
+        trustedLb.text = NSLocalizedString("Trusted", comment: "Verification scene success text")
     }
 
     /**
-     Callback, when user hit the "match" button.
+     Callback, when user hits the "match" button.
+
+     Sets the trust level of the OMEMO device key resp. the OTR fingerprint to `.trustedUser` and
+     stores it.
+
+     Will fade-in a green overlay displaying a checkmark and the text "Trusted".
+
+     When animation is finished, dismisses this view controller.
     */
     @IBAction func match() {
+        if let device = fingerprintContainer as? OTROMEMODevice {
+            device.trustLevel = .trustedUser
+            store(device)
+        }
+        else if let fingerprint = fingerprintContainer as? OTRFingerprint {
+            fingerprint.trustLevel = .trustedUser
+            store(fingerprint)
+        }
+
+        if let window = view.window {
+            overlayContainer.frame = window.frame
+            window.addSubview(overlayContainer)
+            UIView.transition(
+                with: overlayContainer,
+                duration: 0.5,
+                options: .transitionCrossDissolve,
+                animations: {
+                    self.overlayContainer.isHidden = false
+                },
+                completion: { (_) in
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        self.dismiss(animated: true) {
+                            self.overlayContainer.removeFromSuperview()
+                            self.overlayContainer.isHidden = true
+                        }
+                    }
+                })
+        }
     }
 
     /**
-     Callback, when user hit the "Codes don't match" button.
+     Callback, when user hits the "Codes don't match" button.
+
+     Sets the trust level of the OMEMO device key resp. the OTR fingerprint to `.untrusted` resp.
+     `.untrustedUser` and stores it.
+
+     Then dismisses this view controller.
     */
     @IBAction func noMatch() {
+        if let device = fingerprintContainer as? OTROMEMODevice {
+            device.trustLevel = .untrusted
+            store(device)
+        }
+        else if let fingerprint = fingerprintContainer as? OTRFingerprint {
+            fingerprint.trustLevel = .untrustedUser
+            store(fingerprint)
+        }
+
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    /**
+     Store a given `OTROMEMODevice` to the database.
+    */
+    private func store(_ device: OTROMEMODevice) {
+        if let db = OTRDatabaseManager.sharedInstance().readWriteDatabaseConnection {
+            db.asyncReadWrite() { (transaction) in
+                transaction.setObject(device, forKey: device.uniqueId,
+                                      inCollection: type(of: device).collection)
+            }
+        }
+    }
+
+    /**
+     Store a given `OTRFingerprint` to wherever OTRKit stores its fingerprints.
+    */
+    private func store(_ fingerprint: OTRFingerprint) {
+        OTRProtocolManager.encryptionManager.otrKit.save(fingerprint)
     }
 
     /**
