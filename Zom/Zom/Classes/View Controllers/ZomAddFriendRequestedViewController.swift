2f0cebc9564c7b35b70a69703d2c5565b63a0c22
N-Pex <npexdev@gmail.com>
2018-04-16 22:49:09 +0200

    Fix fade out of "Added" view
    Issue #584. Also, change wording from "Requested" to "Added".

--- Zom/Zom/Classes/View Controllers/ZomAddFriendRequestedViewController.swift
+++ Zom/Zom/Classes/View Controllers/ZomAddFriendRequestedViewController.swift
@@ -12,6 +12,14 @@ class ZomAddFriendRequestedViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Dismiss after 3 seconds
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
     @IBAction func didTapView(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
